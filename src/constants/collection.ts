4db103a6106c92ea202ca8cf929e9cf061e208a3
olaf <olaf@olafui-MacBookAir.local>
2023-10-27 13:33:26 +0900

    feat: 데이터 준비

---
+++ src/constants/collection.ts
@@ -0,0 +1,3 @@
+export const COLLECTIONS = {
+  EVENT_BANNER: 'EVENT_BANNER',
+}
---
+++ src/mock/banner.ts
@@ -0,0 +1,20 @@
+export const EVENT_BANNERS = [
+  {
+    title: '계좌 개설하고 캐시백 받기',
+    subTitle: '자산 데이터를 쉽고 빠르게',
+    hasAccount: false,
+    backgroundColor: '#e2f1eb',
+    link: '/event/new-account',
+    iconUrl:
+      'https://cdn1.iconfinder.com/data/icons/filled-line-christmas-icons/75/_card_christmas-512.png',
+  },
+  {
+    title: '지금처럼 소비해도',
+    subTitle: '혜택이 더 큰 카드는',
+    hasAccount: true,
+    backgroundColor: '#877af5d6',
+    link: '/event/recommend-card',
+    iconUrl:
+      'https://cdn4.iconfinder.com/data/icons/business-1219/24/Bank_Card-512.png',
+  },
+]

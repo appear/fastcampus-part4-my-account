5d6447fb70ac776b7ff9514cb83a7b1b53dd8085
olaf <olaf@olafui-MacBookAir.local>
2023-10-29 20:44:28 +0900

    feat: 신용점수

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,17 @@ import dynamic from 'next/dynamic'
 
 import Account from '@components/home/Account'
 import { BannerSkeleton } from '@components/home/EventBanners'
+import { CreditScoreSkeleton } from '@components/home/CreditScore'
+import Spacing from '@shared/Spacing'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   ssr: false,
   loading: () => <BannerSkeleton />,
+})
+
+const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
 })
 
 export default function Home() {
@@ -13,6 +20,8 @@ export default function Home() {
     <>
       <EventBanners />
       <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
     </>
   )
 }

6ad9c460462a976d803826de0874806d432d9b26
olaf <olaf@olafui-MacBookAir.local>
2023-10-29 14:36:24 +0900

    feat: 홈- 내 자산

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,18 @@
 import dynamic from 'next/dynamic'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
 
-import Skeleton from '@shared/Skeleton'
+import Account from '@components/home/Account'
+import { BannerSkeleton } from '@components/home/EventBanners'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   ssr: false,
-  loading: () => (
-    <Skeleton width="100%" height={100} style={{ borderRadius: 8 }} />
-  ),
+  loading: () => <BannerSkeleton />,
 })
 
 export default function Home() {
   return (
-    <Container>
+    <>
       <EventBanners />
-      <div css={bold}>Hello</div>
-    </Container>
+      <Account />
+    </>
   )
 }
-
-const Container = styled.div`
-  background-color: pink;
-`
-
-const bold = css`
-  font-weight: bold;
-`

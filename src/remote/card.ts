d28936a72bfddc78c92e829594536e70420c38e3
olaf <olaf@olafui-MacBookAir.local>
2023-11-03 00:17:59 +0900

    feat: 카드상세

--- src/remote/card.ts
+++ src/remote/card.ts
@@ -6,6 +6,8 @@ import {
   limit,
   getDocs,
   where,
+  getDoc,
+  doc,
 } from 'firebase/firestore'
 
 import { Card } from '@models/card'
@@ -45,4 +47,13 @@ export async function getSearchCards(keyword: string) {
     id: doc.id,
     ...(doc.data() as Card),
   }))
+}
+
+export async function getCard(id: string) {
+  const snapshot = await getDoc(doc(collection(store, COLLECTIONS.CARD), id))
+
+  return {
+    id: snapshot.id,
+    ...(snapshot.data() as Card),
+  }
 }

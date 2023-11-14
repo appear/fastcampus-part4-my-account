519731dd282459bc3ae6270423fa40c7267f5f35
고석진 <appear.ko@gmail.com>
2023-11-14 17:59:05 +0900

    [lesson 3-6] webvitals
    Co-authored-by: olaf <olaf@olafui-MacBookAir.local>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { Global } from '@emotion/react'
 import { QueryClientProvider, QueryClient, Hydrate } from 'react-query'
 import { SessionProvider } from 'next-auth/react'
+import { useReportWebVitals } from 'next/web-vitals'
 
 import globalSteyls from '@styles/globalStyles'
 import Layout from '@shared/Layout'
@@ -16,6 +17,10 @@ export default function App({
   Component,
   pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log(metric)
+  })
+
   return (
     <Layout>
       <Global styles={globalSteyls} />
